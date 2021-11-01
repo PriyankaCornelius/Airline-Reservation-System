80c55815576e046cd235e1b572285c9d32516067
Swes-1304 <swes1304@gmail.com>
2021-10-31 17:22:34 -0700

    update customer Profile backendApi
    Signed-off-by: Swes-1304 <swes1304@gmail.com>

--- Code/Backend/routes/customerUpdateProfileRoute.js
+++ Code/Backend/routes/customerUpdateProfileRoute.js
@@ -6,12 +6,12 @@ var dbconnection = config.dbconnection;
 const router = express.Router();
 
 router.get('/getuserdetails/:id', function (req, res) {
-  console.log('Inside  getuserprofile');
+  console.log('Inside customer getuserprofile');
   //console.log(req.body);
   const personId = req.params.id;
   console.log(personId);
   dbconnection.query(
-    'SELECT p.f_name, p.l_name, p.dob, p.email, p.contact_country_code, p.contact, p.address, p.profilePicture, c.customer_flyer_num, c.mileage_reward from Persons p JOIN Customers c on c.person_id = p.person_id where p.person_id = ?; ',
+    'SELECT p.f_name,p.m_name, p.l_name, p.dob, p.email, p.contact_country_code, p.contact, p.address, p.profilePicture, c.customer_flyer_num, c.mileage_reward from Persons p JOIN Customers c on c.person_id = p.person_id where p.person_id = ?; ',
     [personId],
     async (err, output, fields) => {
       if (err) {
@@ -24,44 +24,52 @@ router.get('/getuserdetails/:id', function (req, res) {
   );
 });
 
-//   router.post('/updateprofile', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     console.log('Inside  updateprofile');
-//     console.log(req.body);
-//     const _id = req.user._id;
-//     const username = req.body.data.username;
-//     const email = req.body.data.email;
-//     const phonenumber = req.body.data.phonenumber;
-//     const defaultcurrency = req.body.data.defaultcurrency;
-//     const timezone = req.body.data.timezone;
-//     const language = req.body.data.language;
-//     const profilepic = req.body.data.profilephoto;
-//     Users.findOneAndUpdate(
-//       { _id },
-//       {
-//         $set: {
-//           username: username,
-//           email: email,
-//           userphone: phonenumber,
-//           usercurrency: defaultcurrency,
-//           userlanguage: language,
-//           usertimezone: timezone,
-//           userprofilephoto: profilepic,
-//         },
-//       },
-//       { new: true }
-//     )
-//       .then((user) =>
-//         res.status(200).send({
-//           username: username,
-//           user_id: _id,
-//           email: email,
-//           profilepic: profilepic,
-//           currencydef: defaultcurrency,
-//         })
-//       )
-//       .catch((err) => {
-//         console.log(err);
-//         res.status(400).send('Error!');
-//       });
-//   });
+router.post('/customerUpdateProfile', (req, res) => {
+  console.log('Inside  customer updateprofile');
+  console.log(req.body);
+  const person_id = req.body.person_id;
+  const f_name = req.body.f_name;
+  const m_name = req.body.m_name;
+  const l_name = req.body.l_name;
+  const dob = req.body.dob;
+  const email = req.body.email;
+  const contact_country_code = req.body.contact_country_code;
+  const contact = req.body.contact;
+  const address = req.body.address;
+  const profilePicture = req.body.profilePicture;
+  sqlquery =
+    "UPDATE Persons SET f_name = '" +
+    f_name +
+    "' , m_name = '" +
+    m_name +
+    "' , l_name = '" +
+    l_name +
+    "' , dob = '" +
+    dob +
+    "' , email = '" +
+    email +
+    "' , contact_country_code = '" +
+    contact_country_code +
+    "' , contact = '" +
+    contact +
+    "' , address = '" +
+    address +
+    "' , profilePicture = '" +
+    profilePicture +
+    "' WHERE person_id = " +
+    person_id;
+
+  dbconnection.query(sqlquery, (err, output, fields) => {
+    if (err) {
+      res.status(400).send('Error!');
+    } else {
+      req.session.cookie.email = email;
+      res.status(200).send({
+        email: email,
+        profilePicture: profilePicture,
+      });
+    }
+  });
+});
+
 module.exports = router;
