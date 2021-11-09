8dbf6e30f776eb39bdf0af491710ddb02389dde0
Swes-1304 <swes1304@gmail.com>
2021-11-08 23:38:07 -0800

    Revert "Update Profile front end"
    This reverts commit 5d375d87fcf0c6235dacd72780497964aa182c88.

--- Code/Frontend/src/components/Main.js
+++ Code/Frontend/src/components/Main.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
-import Profilepage from './profilePage/profilePage';
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+
+//<Route path="/" component={Navbar}/>
+//<Route path="/login" component={Login}/>
 
 class Main extends Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          {/* <Route exact path="/" component={LandingPage} /> */}
-          <Route path='/profile' component={Profilepage} />
-        </Switch>
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div>
+                {/*Render Different Component based on Route*/}
+               
+                   
+            </div>
+        )
+    }
 }
-// Export The Main Component
+
 export default Main;
