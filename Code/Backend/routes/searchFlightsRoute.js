7ee8077e5dd84b581345202125cc1ceadc98f56c
Swes-1304 <swes1304@gmail.com>
2021-11-15 22:07:29 -0800

    earch fligth api backend
    Signed-off-by: Swes-1304 <swes1304@gmail.com>

--- Code/Backend/routes/searchFlightsRoute.js
+++ Code/Backend/routes/searchFlightsRoute.js
@@ -4,22 +4,20 @@ var config = require('../store/config');
 var dbconnection = config.dbconnection;
 const router = express.Router();
 
-router.get('/getflights/:id', function (req, res) {
-  console.log('Inside customer getuserprofile');
+router.get('/getflights/', function (req, res) {
+  console.log('Inside search flights');
   //console.log(req.body);
-  const personId = req.params.id;
-  console.log(personId);
-  dbconnection.query(
-    'SELECT p.f_name,p.m_name, p.l_name, p.dob, p.email, p.contact_country_code, p.contact, p.address, p.profilePicture, c.customer_flyer_num, c.mileage_reward from Persons p JOIN Customers c on c.person_id = p.person_id where p.person_id = ?; ',
-    [personId],
-    async (err, output, fields) => {
-      if (err) {
-        res.status(400).send('Error!');
-      } else {
-        console.log(output);
+  const origin = req.body.origin;
+  const destination = req.body.destination;
+  const departureDate = req.body.departureDate;
+  const arrivalDate = req.body.arrivalDate;
+  dbconnection.query('SELECT * FROM Flights;', async (err, output, fields) => {
+    if (err) {
+      res.status(400).send('Error!');
+    } else {
+      console.log(output);
 
-        res.status(200).send(output);
-      }
+      res.status(200).send(output);
     }
-  );
+  });
 });
